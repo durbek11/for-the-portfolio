b1164003ec3e3cb1cb5d19620286376ae6b374b8
Durbek <pattoyevd@gmail.com>
2022-10-15 17:33:17 +0500

    load link

--- new_app/urls.py
+++ new_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'new_project'
+
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('contact/', contact, name='contact')
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -3,3 +3,6 @@ from .models import *
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def contact(request):
+    return render(request, 'includes/contact.html')
