b9134dcbf05abf9e076b676b8f66ada8a75f9b97
Durbek <pattoyevd@gmail.com>
2022-10-16 22:58:25 +0500

    inital commit

--- new_app/views.py
+++ new_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'includes/index.html')
 
 def contact(request):
     return render(request, 'includes/contact.html')
