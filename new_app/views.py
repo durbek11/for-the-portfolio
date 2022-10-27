9945dc43045cb99a64174df9a1643a7397e02cfc
Durbek <pattoyevd@gmail.com>
2022-10-27 17:41:46 +0500

    add comment

--- new_app/views.py
+++ new_app/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect, get_object_or_404
 from .models import *
 from .form import *
 
 def home(request):
+
+
+    date = Date.objects.all()
+
     context = {
         "date": date, 
          "products":products,
@@ -12,8 +16,32 @@ def home(request):
 def contact(request):
     return render(request, 'includes/contact.html')
 
-def single(request):
-    return render(request, 'includes/single.html')
+def single(request, pk):
+    which_one = Product.objects.get(id=pk)
+    post = get_object_or_404(Product, id=pk)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect('/#product')
+    else:
+        comment_form = CommentForm()
+
+    context= {
+        'new_comment': new_comment,
+        'comments': comments,
+        'comment_form': comment_form,
+        "which_one":which_one
+        }
+   
+    return render(request, "includes/single.html", context)
+   
+
+    
 
 def category(request):
     return render(request, 'includes/category.html')
