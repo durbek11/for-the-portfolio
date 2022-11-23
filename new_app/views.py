646680c48e234cf3c2ffa275e6b30818998ac4e2
Durbek <pattoyevd@gmail.com>
2022-11-23 17:30:18 +0500

    category

---
+++ new_app/views.py
@@ -0,0 +1,90 @@
+from itertools import product
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.views import generic
+from .form import *
+from django.db.models import Q
+from django.http import JsonResponse
+
+
+def home(request):
+   date = Date.objects.all()
+
+   context = {
+        "date": date,
+    }
+   return render(request, 'includes/index.html', context)
+
+def arrivals_detail(request, pk):
+    arrivals_detalis = Arrival.objects.get(id=pk)
+
+    context ={
+        "arrivals_detalis":arrivals_detalis
+    }
+
+    return render(request, "detalis/my-category.html", context)
+    
+def contact(request):
+    return render(request, 'includes/contact.html')
+
+def single(request):
+    
+    return render(request, "includes/single.html")
+
+def cat(request):
+    category = request.GET.get('category')
+    if category == None:
+        arrivals = Arrival.objects.all()
+    else:
+        arrivals = Arrival.objects.filter(category__category_name=category)
+
+
+    if 'product' in request.GET:
+        search = request.GET['product']
+        full_search = Q(Q(title__icontains=search) | Q(Q(price__icontains=search)))
+        products = Product.objects.filter(full_search)
+    else:
+        products = Product.objects.all()
+    
+    
+    if request.method  == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return  redirect('furn:succses')
+    else:
+        form = ContactForm()
+
+    
+   
+    base = Carousel.objects.all()
+    blog = Blog.objects.all()
+    categires = Category.objects.all()
+    context = {
+        "base": base, 
+        "arrivals":arrivals, 
+        "blog":blog,
+        "products":products,
+        "categoryes":categires,
+        "form":form
+    }
+    return render(request, "includes/cat.html")
+   
+
+    
+
+def category(request):
+    return render(request, 'includes/category.html')
+
+def signup(request):
+    form = Registration()
+    if request.method == "POST":
+        form = Registration(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/Login/')
+    
+    return render(request, 'registration/signup.html', {"form":form})
+
+
+
